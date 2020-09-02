9f228b08ef1e1d06244343665b402b008857712d
ATMECS\Abuthahir.Musthafa <abuthahir.musthafa@atmecs.com>
2020-09-02 12:00:34 +0530

    Commit

--- src/main/java/exception/calci/Evaluation.java
+++ src/main/java/exception/calci/Evaluation.java
@@ -2,34 +2,26 @@ package exception.calci;
 
 import java.util.Scanner;
 
-public class Evaluation {
-	static boolean isOperand(char character) {
-		return (character >= '0' && character <= '9');
-	}
+public class Evaluation extends Calculator {
 
-	static int value(char character) {
-		return (int) (character - '0');
-	}
+	public static int evaluate(String expression) {
 
-	static int evaluate(String expression) {
-		if (expression.length() == 0)
-			return -1;
-		int result = value(expression.charAt(0));
-		for (int i = 1; i < expression.length(); i += 2) {
-			char operator = expression.charAt(i);
-			char operand = expression.charAt(i + 1);
-			if (isOperand(operand) == false)
-				return -1;
-			if (operator == '+')
-				result = result + value(operand);
-			else if (operator == '-')
-				result = result - value(operand);
-			else if (operator == '*')
-				result = result * value(operand);
-			else if (operator == '/')
-				result = result / value(operand);
+		String operands[] = expression.split("[\\+\\-\\*\\/]");
+		String operators[] = expression.split("\\d+");
+		result = 0;
+
+		for (index = 0; index < operators.length; index++) {
+			int currentValue = Integer.parseInt(operands[index]);
+			if (operators[index].equals("+"))
+				result = result + currentValue;
+			else if (operators[index].equals("-"))
+				result = result - currentValue;
+			else if (operators[index].equals("*"))
+				result = result * currentValue;
+			else if (operators[index].equals("/"))
+				result = result / currentValue;
 			else
-				return -1;
+				result = result + currentValue;
 		}
 		return result;
 	}
@@ -37,14 +29,11 @@ public class Evaluation {
 	void getResult() {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Input your Expression");
-		String expression2 = input.next();
-		int result = evaluate(expression2);
-		if (result == -1) {
-			System.out.println("Expression Invalid");
-		} else {
-			System.out.println("Value of the expression is " + result);
-			System.out.println("========================================");
-		}
+		String expression = input.next();
+		int result = evaluate(expression);
+		System.out.println("Value of the expression is " + result);
+		System.out.println("========================================");
 
 	}
+
 }
